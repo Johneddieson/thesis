75caa02e08ec8fdefd129bf1cbd720b10da94b52
Johneddieson <vergaraeddieson@gmail.com>
2021-07-15 15:25:23 +0800

    wew

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,46 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {AuthGuard} from  '../app/auth.guard'
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'nurse',
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+      },
+    ]
   },
+  
+  
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'nurse/home',
     pathMatch: 'full'
   },
+  {
+    path: 'admin',
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'adminpage',
+        loadChildren: () => import('./adminpage/adminpage.module').then( m => m.AdminpagePageModule)
+      },
+    ]
+  },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'login',
+        loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+      }
+    ]
+  }
+  
+ 
 ];
 
 @NgModule({
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import {AuthService} from '../app/auth.service'
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private authservice: AuthService) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.authservice.isLoggedIn) {
+      return true
+    } 
+    this.router.navigateByUrl('/auth/login')
+    return false
+  }
+  
+}

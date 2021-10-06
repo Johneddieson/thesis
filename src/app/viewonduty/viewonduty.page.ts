4d9c7969ed846ad84734e170e9da5ce8fa0f469e
Johneddieson <vergaraeddieson@gmail.com>
2021-10-07 01:26:49 +0800

    dati parin

--- src/app/viewonduty/viewonduty.page.ts
+++ src/app/viewonduty/viewonduty.page.ts
@@ -27,7 +27,9 @@ export class ViewondutyPage implements OnInit, AfterViewInit{
     { lat: 47.92393, lng: 78.58339 }
   ];
   constructor(private afstore: AngularFirestore) { 
-   
+    this.details = JSON.parse(sessionStorage.getItem('user'))
+    
+    this.meRefence = this.afstore.collection('users').doc(`${this.details.uid}`)
   }
 ngAfterViewInit() {
   // var mark = [
@@ -55,73 +57,30 @@ ngAfterViewInit() {
   // });
 }
   ngOnInit() {
-    // let mapOptions = {
-    //    center: 
-    //    {lat: 17.6022296, lng: 121.6894202},
-    //    zoom: 10,
+    let mapOptions = {
+       center: 
+       {lat: 17.6022296, lng: 121.6894202},
+       zoom: 10,
      
-    //    disableDefaultUI: true,
-    //    mapTypeId: google.maps.MapTypeId.ROADMAP
-    //  }
-    //  this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-//     this.details = JSON.parse(sessionStorage.getItem('user'))
+       disableDefaultUI: true,
+       mapTypeId: google.maps.MapTypeId.ROADMAP
+     }
+     this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
     
-//     this.meRefence = this.afstore.collection('users').doc(`${this.details.uid}`)
    
     
-// this.sub = this.meRefence.valueChanges()
-// .subscribe(data => {
+this.sub = this.meRefence.valueChanges()
+.subscribe(data => {
 
-//   this.trackedRoute.push({lat: data.latitude, lng: data.longitude})
+  this.trackedRoute.push({lat: data.latitude, lng: data.longitude})
  
-// this.redrawPath(this.trackedRoute)
+this.redrawPath(this.trackedRoute)
  
-// })
+})
 
-// this.redrawPath(this.trackedRoute)
-//  var trialLat = new google.maps.LatLng(17.6022296, 121.6894202)
-//  this.map.setCenter(trialLat);    
-        
-var mark = [
-  { lat: 22.33159, lng: 105.63233 },
-  { lat: 7.92658, lng: -12.05228 },
-  { lat: 48.75606, lng: -118.859 },
-  { lat: 5.19334, lng: -67.03352 },
-  { lat: 12.09407, lng: 26.31618 },
-  { lat: 47.92393, lng: 78.58339 }
-];
-let mapOptions = {
-  center: 
-  {lat: 17.6022296, lng: 121.6894202},
-  zoom: 10,
-
-  disableDefaultUI: true,
-  mapTypeId: google.maps.MapTypeId.ROADMAP
-}
-this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-this.xamplepath(mark)
-// mark.forEach(location => {
-//   var marker = new google.maps.Marker({
-//     position: new google.maps.LatLng(location.lat, location.lng),
-//     map: this.map
-//   });
-// });
-  
-  }
-
-  xamplepath(path) {
-    // var marker = new google.maps.Marker({
-    //   position: new google.maps.LatLng(location.lat, location.lng),
-    //   map: this.map
-    // });
-    path.forEach(location => {
-       var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(location.lat, location.lng),
-      map: this.map
-    });
-      
-    });
+this.redrawPath(this.trackedRoute)
+ var trialLat = new google.maps.LatLng(17.6022296, 121.6894202)
+ this.map.setCenter(trialLat);    
   }
   redrawPath(path) {
   
@@ -131,7 +90,7 @@ this.xamplepath(mark)
     } 
    if (path.length > 1) {
       this.currentMapTrack = new google.maps.Marker({
-       animation: google.maps.Animation.DROP,
+      // animation: google.maps.Animation.DROP,
         position: {lat: path[wew].lat, lng: path[wew].lng},
       
       })
